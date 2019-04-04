a9d0086ff2dbfd3917366023d816811b2a61b63e
Josh H <jshunt196@gmail.com>
2019-04-04 17:35:36 -0600

    "main activity made scrollable"

---
+++ app/src/main/java/com/example/jshun/mybudget/TransactionsActivity.java
@@ -0,0 +1,35 @@
+package com.example.jshun.mybudget;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class TransactionsActivity extends AppCompatActivity {
+    public TransactionsActivity() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // TODO: stuff here!
+        // setContentView(R.layout.activity_main);
+    }
+
+    /*@Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.toolbar_map, menu);
+    }*/
+}
