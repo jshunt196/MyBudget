b9d7936f9f11de40e83a59a36c40fa4b8b3b42b9
Donny Skousen <donny.skousen@gmail.com>
2019-04-15 22:19:23 -0600

    fixed add transaction button on transaction activity

--- app/src/main/java/com/example/jshun/mybudget/TransactionsActivity.java
+++ app/src/main/java/com/example/jshun/mybudget/TransactionsActivity.java
@@ -39,7 +39,7 @@ public class TransactionsActivity extends AppCompatActivity {
         float semesterIncome = theStuff.getSemesterIncome();
         float semesterExpenses = theStuff.getSemesterExpenses();
         float semesterBudgetedExpenses = theStuff.getSemsterBudgetedExpenses();
-        float remainderBudgeted = semesterExpenses - semesterBudgetedExpenses;
+        float remainderBudgeted = semesterBudgetedExpenses - semesterExpenses;
         TextView incomeView = findViewById(R.id.IncomeView);
         incomeView.setText(String.valueOf(semesterIncome));
         TextView remainder = findViewById(R.id.BudgetRemainingView);
@@ -62,6 +62,10 @@ public class TransactionsActivity extends AppCompatActivity {
                 //Toast toast = Toast.makeText(getApplicationContext(), "Finished!!!", Toast.LENGTH_LONG);
                 //toast.show();
                 Intent i = new Intent(TransactionsActivity.this, AddTransactionFrag.class);
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("income", false);
+                bundle.putInt("index", 0);
+                i.putExtras(bundle);
                 startActivity(i);
             }
         });
